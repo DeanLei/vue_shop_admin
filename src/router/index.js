36cddf849b0b704718df2604fc47f22a582b7f00
DeanLei <1244171060@qq.com>
2021-08-07 13:51:18 +0800

    login功能完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/",
+    redirect: "/login"
+  },{
+    path: "/login",
+    component: () => import("@/components/Login")
+  },{
+    path: "/home",
+    component: () => import("@/components/Home")
   }
 ]
 
@@ -24,4 +20,19 @@ const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  if(to.path === '/login') {
+    next();
+    return;
+  }
+  const tokenStr = sessionStorage.getItem('token');
+  if(!tokenStr) {
+    next('/login')
+    return
+  }
+  next();
+  // from 代表从哪个路径来
+  // next 表示放行， next('/xxx')强制跳转某个路径
+})
 export default router
