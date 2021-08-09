b5f7af5276c1a9dbbf81e368a060a86e4a2322ea
DeanLei <1244171060@qq.com>
2021-08-09 16:55:34 +0800

    完成了分类功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -27,6 +27,9 @@ const routes = [
       },{
         path: "/roles",
         component: () => import("@/views/Power/Roles")
+      },{
+        path: "/categories",
+        component: () => import("@/views/Goods/Cate")
       }
     ]
   }
